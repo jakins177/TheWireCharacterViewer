ecb152d742aaca618d5eb0f0bd6e1f9811bdb8f5
joshakins <jakins177@email.com>
2023-06-29 17:13:01 -0500

    added back button functionality

--- app/src/main/java/com/sample/wireviewer/view/MainActivity.kt
+++ app/src/main/java/com/sample/wireviewer/view/MainActivity.kt
@@ -2,6 +2,10 @@ package com.sample.wireviewer.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.sample.wireviewer.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
